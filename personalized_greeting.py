a0dcc5affefed408a20faaa0fe252a793a389df1
KajalDangi <kajaldangithakur@gmail.com>
2025-10-26 11:51:17 +0530

    Create personalized_greeting.py

---
+++ personalized_greeting.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+
+first_name=input("Enter your first name: ")
+second_name=input("Enter your second name: ")
+full_name=first_name+" "+second_name
+print(f"Hello,{full_name} ! Welcome to the Python program.")
