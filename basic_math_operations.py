859e67f85dbea31ee9d1c1ec3f5879c67a3cc7c0
KajalDangi <kajaldangithakur@gmail.com>
2025-10-26 11:51:41 +0530

    Update basic_math_operations.py

--- basic_math_operations.py
+++ basic_math_operations.py
@@ -1,4 +1,4 @@
-Task 1: Perform Basic Mathematical Operations
+#Task 1: Perform Basic Mathematical Operations
 
 first_number=int(input("Enter the first number: "))
 second_number=int(input("Enter the second number: "))
