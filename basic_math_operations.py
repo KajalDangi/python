cb339184f657ebaeb94472fb36ff535a8fad2fbf
KajalDangi <kajaldangithakur@gmail.com>
2025-10-26 11:48:40 +0530

    Create basic_math_operations.py

---
+++ basic_math_operations.py
@@ -0,0 +1,8 @@
+Task 1: Perform Basic Mathematical Operations
+
+first_number=int(input("Enter the first number: "))
+second_number=int(input("Enter the second number: "))
+print("Addition:",first_number+second_number)
+print("Subtraction:",first_number-second_number)
+print("Multiplication:",first_number*second_number)
+print("Division:",first_number/second_number)
